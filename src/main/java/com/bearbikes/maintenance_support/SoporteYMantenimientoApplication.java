c1ccf5903a6e437f18b677cd0a09fff23c6ffa24
Brdn08 <93061195+Brnd08@users.noreply.github.com>
2023-03-31 20:50:11 -0600

    Se creo la estructura y configuracion inicial del proyecto

---
+++ src/main/java/com/bearbikes/maintenance_support/SoporteYMantenimientoApplication.java
@@ -0,0 +1,13 @@
+package com.bearbikes.maintenance_support;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoporteYMantenimientoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoporteYMantenimientoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bearbikes/maintenance_support/SoporteYMantenimientoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bearbikes.maintenance_support;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoporteYMantenimientoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
